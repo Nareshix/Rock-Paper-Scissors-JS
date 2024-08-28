8d39df526b1823f1d68c06d1d550da35c7dffdc5
nareshix <nareshix66@gmail.com>
2024-08-28 23:13:14 +0800

    converted the human choice to lower case

--- javascript.js
+++ javascript.js
@@ -3,7 +3,14 @@ const getComputerChoice = () => {
     const index = Math.round(Math.random() * 2)
     return choice[index];
 }
-const humanChoice = prompt();
-
+const humanChoice = prompt().toLowerCase();
 let humanScore = 0;
 let computerScore  = 0;
+
+
+
+// const won = {'paper': ['stone', 'scissor']}
+// const playRound = (humanChoice, getComputerChoice()) => {
+//     if (humanChoice)
+// }   
+
